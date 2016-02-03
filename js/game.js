5ea6fde0712031c6555309baa0a970ebaa18715d
Harry Ganz <harry.y.ganz@gmail.com>
2016-02-02 20:29:56 -0500

    Create ship object and move it around game grid
    Created a ship constructor with a move function that changes its x
    position as well as a grid in the game object and a moveShip method that
    moves the ship around the grid

--- js/game.js
+++ js/game.js
@@ -4,10 +4,41 @@
 var $gameWindow;
 var $startBtn;
 
+function Ship(x){
+  this.x = x || 0;
+  this.move = function(x){
+    this.x = x;
+  };
+}
+
 // Game object
 var game = {
+  ship: {},
+  grid: [],
   start: function(){
-    console.log('Game Started');
+    // Create 11 element grid array
+    this.grid = new Array(11);
+    this.grid.fill(null);
+    // Create a new ship and add make it this.ship
+    this.ship = new Ship();
+    // Place ship on grid at center position
+    this.moveShip(5);
+  },
+  moveShip: function(dx){
+    // If ship will go beyond edge of grid
+    // do nothing
+    // otherwise move the ship to the new location
+    var xAfterMove = this.ship.x + dx;
+    if(xAfterMove < 0 || xAfterMove > (this.grid.length-1)){
+      return ;
+    } else {
+      // Remove ship from previous position
+      this.grid.splice(this.ship.x, 1);
+      // Update ships posistion
+      this.ship.move(xAfterMove);
+      // Put ship in new grid position
+      this.grid.splice(this.ship.x, 0, this.ship);
+    }
   }
 };
 
