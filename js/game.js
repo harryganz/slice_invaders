a2adbcdc4939fb69b8351f9141bf1fe6adf53afa
Harry Ganz <harry.y.ganz@gmail.com>
2016-02-02 19:38:31 -0500

    Add start button listener

--- js/game.js
+++ js/game.js
@@ -1,6 +1,26 @@
 (function() {
   'use strict';
+  // Html Elements
+  var $gameWindow;
+  var $startBtn;
+
+
+  function startHandler(event){
+    // Hide the start button
+    $startBtn.hide();
+  }
+
+
     $(document).ready(function(){
       console.log('Jquery and game.js Loaded');
+      // Load html elements
+      $gameWindow = $('#game-window');
+      $startBtn = $('#start-btn');
+      // On click run the start handler
+      $startBtn.click(startHandler);
+
+
     });
+
+
 }());
