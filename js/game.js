7d11cf87bd51ebb61b1b9145deec218f4ad6d574
Harry Ganz <harry.y.ganz@gmail.com>
2016-02-03 10:42:29 -0500

    Move ship with A and D keys
    Move ship on bottom of game window using the A and D keys
    Handle collisions with edge of game window

--- js/game.js
+++ js/game.js
@@ -7,16 +7,20 @@ var $startBtn;
 // Game Elements
 var $ship;
 
-function Ship(x, y, width){
+function Ship(x, y, width, height){
   this.x = x || 0;
   this.y = y || 0;
   this.width = width || 1;
+  this.height = height || 1;
   this.move = function(x, y){
     this.x = x || 0;
     this.y = y || 0;
   };
   this.setWidth = function(width){
     this.width = width || 1;
+  };
+  this.setHeight = function(height){
+    this.height = height || 1;
   };
 }
 
@@ -31,15 +35,15 @@ var game = {
     this.height = height || 100;
     // Create a new ship and add make it this.ship
     // Place in center of board
-    this.ship = new Ship(this.width/2, 0);
+    this.ship = new Ship(this.width/2, 0, 20, 20);
   },
   moveShip: function(dx){ // Move ship by dx on board
     // If ship will be off the board move to edge of board
     var xAfterMove = this.ship.x + dx;
     if(xAfterMove < 0){
       this.ship.move(0);
-    } else if (xAfterMove > this.width) {
-      this.ship.move(this.width);
+    } else if (xAfterMove > (this.width - this.ship.width)) {
+      this.ship.move(this.width - this.ship.width);
     } else {
       this.ship.move(xAfterMove);
     }
@@ -58,12 +62,18 @@ function initializeBoard(){
   $gameWindow.append($ship);
   // Draw the ship
   drawShip(game.ship);
+  // On keypress run the moveShip handler
+  $(document).on('keydown', moveShip);
 }
 
 // Draw the ship
 // Has an absolute position relative to the game board
 function drawShip(ship){
-  $ship.css('bottom', 0).css('left', ship.x);
+  $ship.
+  css('width', ship.width).
+  css('height', ship.height).
+  css('bottom', 0).
+  css('left', ship.x);
 }
 
 // Draw the game and all its objects
@@ -76,6 +86,22 @@ function startHandler(event){
   initializeBoard();
 }
 
+// Handler for pressing a and d key
+function moveShip(event){
+   // a 65 d 68
+   // When a is pressed move ship left by 10 pixels
+   if(event.which === 65){
+     game.moveShip(-30);
+   }
+   // When d is pressed move ship right 10 pixel
+   if(event.which === 68){
+     game.moveShip(30);
+   }
+
+   // Draw ship again
+   drawShip(game.ship);
+}
+
 $(document).ready(function(){
   console.log('Jquery and game.js Loaded');
   // Load html elements
@@ -83,6 +109,7 @@ $(document).ready(function(){
   $startBtn = $('#start-btn');
   // On click run the start handler
   $startBtn.click(startHandler);
+
 });
 
 }());
